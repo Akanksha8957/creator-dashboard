3751ec6117fe9729832bed8ea28a33026a079bc1
Akanksha Prajapati <104263399+Akanksha8957@users.noreply.github.com>
2026-02-11 14:28:48 +0530

    Fix final relative imports for flat structure

--- auth.py
+++ auth.py
@@ -4,7 +4,7 @@ from jose import JWTError, jwt
 from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from . import models, schemas
+import models, schemas
 import os
 from dotenv import load_dotenv
 
